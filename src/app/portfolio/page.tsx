c927c1c0b1e742570e8bc90646ff2f32516283bc
Travis B. Mitchell <hi@chemeonix.com>
2025-08-16 17:17:58 -0700

    refactor: Move portfolio to /portfolio to create new landing page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,18 @@
+// File: src/app/page.tsx
+import { HeroSection } from "@/components/sections/hero-section";
+import { AboutSection } from "@/components/sections/about-section";
+import { ExperienceSection } from "@/components/sections/experience-section";
+import { SkillsSection } from "@/components/sections/skills-section";
+import { PublicationsSection } from "@/components/sections/publications-section";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <SkillsSection />
+      <PublicationsSection />
+    </div>
+  );
+}
